d9db508919d1844db8b60ba39692515feaccc9b9
Nandomk <nandomk@gmail.com>
2024-11-01 21:07:21 -0300

    add metodos

--- examples/TesteFinanceiro.py
+++ examples/TesteFinanceiro.py
@@ -2,11 +2,10 @@ import sys
 import os 
 
 sys.path.append(os.path.abspath('..'))
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cwd = os.getcwd()
 
-from libs.Financeiro import calcular_valor_futuro, calcular_valor_presente
+from libs.Financeiro import calcular_valor_futuro, calcular_valor_presente, calcular_taxa_juros, calcular_periodos
 
 # Exemplo de uso
 principal = 1000  # Valor inicial
@@ -20,3 +19,20 @@ print(f"O valor futuro após {periodos} períodos é de R${valor_futuro:.2f}")
 valor_futuro_desejado = 2000  # Valor futuro desejado
 valor_presente = calcular_valor_presente(valor_futuro_desejado, taxa_juros, periodos)
 print(f"O valor presente necessário para atingir R${valor_futuro_desejado:.2f} após {periodos} períodos é de R${valor_presente:.2f}")
+
+
+ # Exemplo de chamada do método calcular_taxa_juros
+valor_presente = 1000  # Valor inicial investido
+valor_futuro = 2000    # Valor futuro desejado
+periodos = 5           # Número de períodos (anos, meses, etc.)
+
+taxa_juros = calcular_taxa_juros(valor_presente, valor_futuro, periodos)
+print(f"A taxa de juros necessária é: {taxa_juros:.2%}")
+
+# Exemplo de chamada do método calcular_periodos
+valor_presente = 1000  # Valor inicial investido
+valor_futuro = 2000    # Valor futuro desejado
+taxa_juros = 0.05      # 5% de juros
+periodo = calcular_periodos(valor_presente, valor_futuro, taxa_juros)
+print(f"O número de períodos necessários é: {periodo:.2f}")
+
--- libs/Financeiro.py
+++ libs/Financeiro.py
@@ -1,3 +1,6 @@
+import math
+
+
 def calcular_valor_futuro(principal, taxa_juros, periodos):
     """
     Calcula o valor futuro usando juros compostos.
@@ -24,3 +27,27 @@ def calcular_valor_presente(valor_futuro, taxa_juros, periodos):
     valor_presente = valor_futuro / (1 + taxa_juros) ** periodos
     return valor_presente
 
+def calcular_taxa_juros(valor_presente, valor_futuro, periodos):
+        """
+        Calcula a taxa de juros compostos.
+
+        :param valor_presente: Valor presente ou inicial
+        :param valor_futuro: Valor futuro desejado
+        :param periodos: Número de períodos (anos, meses, etc.)
+        :return: Taxa de juros (em decimal)
+        """
+        taxa_juros = (valor_futuro / valor_presente) ** (1 / periodos) - 1
+        return taxa_juros
+
+
+def calcular_periodos(valor_presente, valor_futuro, taxa_juros):
+            """
+            Calcula o número de períodos necessários para atingir um valor futuro com juros compostos.
+
+            :param valor_presente: Valor presente ou inicial
+            :param valor_futuro: Valor futuro desejado
+            :param taxa_juros: Taxa de juros (em decimal, por exemplo, 0.05 para 5%)
+            :return: Número de períodos necessários
+            """
+            periodos = math.log(valor_futuro / valor_presente) / math.log(1 + taxa_juros)
+            return periodos
